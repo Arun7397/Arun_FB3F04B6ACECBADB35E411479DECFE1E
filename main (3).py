7bfe0ead2675427f4ce9789940b4730fa6ef88c3
Arun7397 <144302298+Arun7397@users.noreply.github.com>
2023-10-28 11:22:55 +0530

    Delete main (3).py

--- main (3).py
+++
@@ -1,7 +0,0 @@
-#online python computer interpreters to Fun python online.
-#write python 3 code in this online editor and Fun it. 
-year = 2023
-if(year%4 ==0):
-  print("leap year")
-else:
-  print("Not leap year")
